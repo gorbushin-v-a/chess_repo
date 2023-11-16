1d3a738e0a16a069b3778939d712c5b64ed43a86
tvoloshin <tvoloshin38@gmail.com>
2023-11-16 18:42:33 +0300

    Добавил функции генерации возможных ходов

---
+++ moves.py
@@ -0,0 +1,108 @@
+from itertools import chain
+
+
+NO_PIECE = 0
+PIECES = (W_PAWN, W_KNIGHT, W_BISHOP, W_ROOK, W_QUEEN, W_KING, B_PAWN, B_KNIGHT, B_BISHOP, B_ROOK, B_QUEEN, B_KING) =\
+    list(chain(range(1, 7), range(9, 15)))
+
+piece_string_to_int = {'P': W_PAWN,
+                       'N': W_KNIGHT,
+                       'B': W_BISHOP,
+                       'R': W_ROOK,
+                       'Q': W_QUEEN,
+                       'K': W_KING,
+                       'p': B_PAWN,
+                       'n': B_KNIGHT,
+                       'b': B_BISHOP,
+                       'r': B_ROOK,
+                       'q': B_QUEEN,
+                       'k': B_KING}
+
+
+def get_idx_from_coords(coords):
+    x, y = coords
+    m, n = x, abs(y - 7)
+    return m + 8 * n
+
+
+def is_free(field, coords):
+    return not field[get_idx_from_coords(coords)]
+
+
+def is_allowed(field, coords_to, coords_from):
+    return 0 <= coords_to[0] < 8 and 0 <= coords_to[1] < 8 and (not is_same_color(field, coords_to, coords_from) or is_free(field, coords_to))
+
+
+def is_same_color(field, coords1, coords2):
+    return (field[get_idx_from_coords(coords1)] >> 3) & 1 == (field[get_idx_from_coords(coords2)] >> 3) & 1
+
+
+def get_knight_moves(field, coords):
+    moves = []
+    x, y = coords
+    for i in range(-2, 3):
+        for j in range(-2, 3):
+            # print(x + i, y + j)
+            if abs(i) + abs(j) == 3 and 0 <= x + i < 8 and 0 <= y + j < 8:
+                if not is_same_color(field, (x + i, y + j), coords) or is_free(field, (x + i, y + j)):
+                    moves.append((x + i, y + j))
+    return moves
+
+
+def get_bishop_moves(field, coords):
+    moves = []
+    x, y = coords
+    for i in (-1, 1):
+        for j in (-1, 1):
+            for k in range(1, 8):
+                if 0 <= x + i * k < 8 and 0 <= y + j * k < 8:
+                    if is_free(field, (x + i * k, y + j * k)):
+                        moves.append((x + i * k, y + j * k))
+                    elif not is_same_color(field, coords, (x + i * k, y + j * k)):
+                        moves.append((x + i * k, y + j * k))
+                        break
+                    else:
+                        break
+    return moves
+
+
+def get_pawn_moves(field, coords):
+    moves = []
+    x, y = coords
+
+    dir = 1 if field[get_idx_from_coords(coords)] < 8 else -1
+
+    if is_free(field, (x, y + dir)):
+        moves.append((x, y + dir))
+
+    if (y - dir == 0 or y - dir == 7) and is_allowed(field, (x, y + dir * 2), coords):
+        moves.append((x, y + dir * 2))
+
+    if not is_free(field, (x + 1, y + dir)) and not is_same_color(field, coords, (x + 1, y + dir)):
+        moves.append((x + 1, y + dir))
+    if not is_free(field, (x - 1, y + dir)) and not is_same_color(field, coords, (x - 1, y + dir)):
+        moves.append((x - 1, y + dir))
+
+    return moves
+
+
+if __name__ == "__main__":
+    board_fen = "rnbqkbnr/pppppppp/8/8/8/P7/1PPPPPPP/RNBQKBNR"
+    squares = [None] * 64
+
+    index = 0
+    for char in board_fen:
+        if char in 'KQRBNPkqrbnp':
+            squares[index] = piece_string_to_int[char]
+            index += 1
+        elif char in '12345678':
+            for _ in range(int(char)):
+                squares[index] = NO_PIECE
+                index += 1
+        elif char == '/':
+            pass
+        else:
+            raise Exception("Unknown character in FEN: {}".format(char))
+
+    print(squares)
+    print(get_pawn_moves(squares, (0, 6)))
